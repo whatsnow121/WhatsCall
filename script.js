14bd3252bf9b439d067e24d1aa298a2a4fc8a530
whatsvideo <91049705+whatsnow121@users.noreply.github.com>
2021-09-20 20:39:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  location.hash = Math.floor(Math.random() * 0xFFFFFFFFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
   
